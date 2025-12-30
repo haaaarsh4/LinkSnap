92cdb231bb9b0772011ac02ae03d247dd51f7ad0
haaaarsh4 <harshupadhyay2004@gmail.com>
2025-12-30 18:25:43 -0500

    ICON

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,20 @@
 import "./globals.css";
 import "./globals.css";
 import Navbar from "../component/navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "LinkSnap",
+  description: "URL Shortener & QR Code Generator",
+  icons: {
+    icon: [
+      {
+        url: '/DP.jpg',
+        type: 'image/jpeg',
+      }
+    ],
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
